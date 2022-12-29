94d4b00ae31e24cb1709318a2e2b28ab923d4587
pilotsatyam <satyamsrivastaviit@gmail.com>
2022-12-29 16:50:45 +0530

    Backend added

---
+++ MedicalProjects/backend/medical-apis/src/main/java/com/ImyEye/info/config/SecurityConfig.java
@@ -0,0 +1,139 @@
+package com.ImyEye.info.config;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import com.ImyEye.info.Security.CustomUserDetailService;
+import com.ImyEye.info.Security.JwtAuthenticationEntryPoint;
+import com.ImyEye.info.Security.JwtAuthenticationFilter;
+
+@EnableWebSecurity
+@Configuration
+@EnableWebMvc
+@EnableMethodSecurity(prePostEnabled = true)
+
+public class SecurityConfig{
+
+    // public static final String[] PUBLIC_URLS={
+    //      "/api/v1/auth/login","api/v1/auth/register","/v3/api-docs"
+    // };
+
+    @Autowired
+    private CustomUserDetailService customUserDetailService;
+    @Autowired
+    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+/**
+ * @param http
+ * @return
+ * @throws Exception
+ */
+@Bean
+public SecurityFilterChain SecurityFilterChain(HttpSecurity http) throws Exception{
+    http
+    .csrf()
+    .disable()
+    .authorizeHttpRequests()
+    .requestMatchers("/api/v1/auth/login")
+    // .requestMatchers("/api/v1/auth/**")
+    .permitAll()
+    .requestMatchers("/v3/api-docs")
+    .permitAll()
+    .requestMatchers("/api/v1/auth/register")
+    .permitAll()
+    .requestMatchers(HttpMethod.GET)
+    .permitAll()
+    // .requestMatchers("/v2/api-docs", "/swagger-resources/configuration/ui", "/swagger-resources", "/swagger-resources/configuration/security", "/swagger-ui.html", "/webjars/**")
+    // .permitAll()
+    .requestMatchers("/swagger-resources/**")
+    .permitAll()
+    .requestMatchers("/swagger-ui/**")
+    .permitAll()    
+    .requestMatchers("/webjars/**")
+    .permitAll()
+    .anyRequest()
+    .authenticated()
+    
+    // .requestMatchers("/api/v1/auth/login").authenticated()
+    // .anyRequest().permitAll()
+    .and()
+    .exceptionHandling()
+    .authenticationEntryPoint(this.jwtAuthenticationEntryPoint)
+    .and()
+    .sessionManagement()
+    .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    http.addFilterBefore(this.jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+    http.authenticationProvider(daoAuthenticationProvider());
+    DefaultSecurityFilterChain defaultSecurityChain = http.build();
+    return defaultSecurityChain;
+}
+
+@Bean
+public PasswordEncoder passwordEncoder(){
+    return new BCryptPasswordEncoder();
+}
+
+@Bean
+public DaoAuthenticationProvider daoAuthenticationProvider(){
+DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+provider.setUserDetailsService(this.customUserDetailService);
+provider.setPasswordEncoder(passwordEncoder());
+return provider;
+}
+
+@Bean
+public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration configuration) throws Exception
+{
+    return configuration.getAuthenticationManager();
+}
+
+@Bean
+public FilterRegistrationBean coresFilter() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+    CorsConfiguration corsConfiguration = new CorsConfiguration();
+    corsConfiguration.setAllowCredentials(true);
+    corsConfiguration.addAllowedOriginPattern("*");
+    corsConfiguration.addAllowedHeader("Authorization");
+    corsConfiguration.addAllowedHeader("Content-Type");
+    corsConfiguration.addAllowedHeader("Accept");
+    corsConfiguration.addAllowedMethod("POST");
+    corsConfiguration.addAllowedMethod("GET");
+    corsConfiguration.addAllowedMethod("DELETE");
+    corsConfiguration.addAllowedMethod("PUT");
+    corsConfiguration.addAllowedMethod("OPTIONS");
+    corsConfiguration.setMaxAge(3600L);
+
+    source.registerCorsConfiguration("/**", corsConfiguration);
+
+    FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+
+    bean.setOrder(-110);
+
+    return bean;
+}
+
+
+}
