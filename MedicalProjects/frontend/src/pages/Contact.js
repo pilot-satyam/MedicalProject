4f9d737d4b0f32fe41516700ab90e0fe94c8e758
Sanmati Porlekar <porlekarsanmati@gmail.com>
2023-02-01 12:12:36 +0530

    Working

--- MedicalProjects/frontend/src/pages/Contact.js
+++ MedicalProjects/frontend/src/pages/Contact.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import Base from "../Components/Base";
 import '../Style/Global.css';
-import '../Style/placeholderTop.css';
-import '../Style/backgrd.css'
+
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import {Button, Card,CardHeader,Container, FormGroup,CardBody} from 'reactstrap'
+import Form from 'react-bootstrap/Form';
 import userContext from '../context/userContext';
+
 const Contact=()=>{
+
+
 
   return (
     
-<div>
+<>
   <Base >
-<div className='contact'>
+
+{/* <div className='contact'>
 <h1 className="common-heading d-flex align-items-center justify-content-center mt-5">Contact page &nbsp;
   <i style={{fontSize:"24px"}} className='far'>&#xf086;</i>
 </h1>
@@ -19,13 +25,14 @@ const Contact=()=>{
 
 <div className="container col-4 d-flex align-items-center justify-content-center">
  
-  <div className="contact-form form-container">
+  <div className="contact-form form-container" style={{ width: "40%", margin: "0 auto", textAlign: "center" }}>
     <form 
       action="https://formspree.io/f/myyvrrwa"
       method="POST"
       className="contact-inputs form">
 
-    <div className="user-input-wrp">   
+    <div className="user-input-wrp">  
+    <label className="floating-label">UserName</label> 
       <input
         type="text"
         name="username"
@@ -33,11 +40,11 @@ const Contact=()=>{
         autoComplete="off"
         className='mt-3'
       />
-         <span className="floating-label">UserName</span>
+        
     </div>
 
     <div className="user-input-wrp">
-
+      <label className="floating-label">Email</label>
       <input
         type="email"
         name="Email"
@@ -45,11 +52,12 @@ const Contact=()=>{
         required
         className='mt-3'
       />
-      <span className="floating-label">Email</span>
+     
     </div>
     
 
     <div className="user-input-wrp">
+      <label className="floating-label">Enter Text</label>
       <textarea
         name="Message"
         cols="30"
@@ -59,7 +67,7 @@ const Contact=()=>{
         className='mt-3'>
         
       </textarea>
-      <span className="floating-label">Enter Text</span>
+      
     </div>
 
       <div className="text-center">
@@ -68,11 +76,76 @@ const Contact=()=>{
         </button>
       </div>
     </form>
+
+
+
   </div>
-</div>
-</div>
+</div> */}
+
+
+
+{/* </div> */}
+ <CardHeader className='mt-4 formStyle'>
+ <h1 className="common-heading d-flex align-items-center justify-content-center mt-5">Contact Page &nbsp;
+  <i style={{fontSize:"24px"}} className='far'>&#xf086;</i>
+</h1>
+                </CardHeader>
+<CardBody className='formStyle card-body'>
+ 
+                    <Form  action="https://formspree.io/f/myyvrrwa"
+                            method="POST"
+                            className="contact-inputs form">
+
+<br/>
+  <br/>
+
+                    <FloatingLabel
+                        controlId="floatingInput"
+                        label="Username"
+                        className="mb-3"
+                        
+                    >
+                        <Form.Control
+                         type="text" 
+                         placeholder="Username" 
+                         name="username"
+                         />
+                    </FloatingLabel>
+                    <FloatingLabel
+                        controlId="floatingInput"
+                        label="Email"
+                        className="mb-3"
+                    >
+                        <Form.Control
+                         type="email" 
+                         placeholder="Email" 
+                         name="Email"
+                         />
+                    </FloatingLabel>
+
+
+
+                    <FloatingLabel controlId="floatingPassword" label="Enter Message">
+                        <Form.Control 
+                        type="textarea" 
+                        placeholder = "textarea" 
+                        name="Message"
+                        
+                         />
+                    </FloatingLabel>
+
+                    <br></br>
+                  
+                <Container className='text-center'>
+                    <Button color="primary" outline>Submit</Button>
+                </Container>
+                <br/>
+  <br/>
+                    </Form>
+                </CardBody>
 </Base>
-</div>
+</>
+
   )
 }
 
